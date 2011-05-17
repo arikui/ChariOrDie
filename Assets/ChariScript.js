af653e4b3c54855856e8c675c6243af0f8a5926a
arikui <arikui0gl@gmail.com>
2011-05-18 03:06:52 +0900

    add street lump

--- Assets/ChariScript.js
+++ Assets/ChariScript.js
@@ -11,29 +11,16 @@ function Start () {
 	myTransform = transform;
     // Find the OtherScript which is attached to any game object in the scene.
     other = FindObjectOfType(GUIScript) as GUIScript;
-	startPosition = transform.position;
+	startPosition = myTransform.position;
 }
 
 function Update() {
-    myTransform.Rotate(0, speed, 0);
+    myTransform.Rotate(0, -speed, 0);
     myTransform.Translate(speed, 0, 0);
 	
 }
 
 function Restart() {
 	myTransform.position = startPosition;
-	myTransform.rotation = Quaternion(0, 0, 0, 0);
-}
-
-function OnCollisionStay(collisionInfo : Collision) {
-	//~ transform.Rotate(0, other.speed, 0);
-    //~ transform.Translate(other.speed, 0, 0);
-	//~ rigidbody.AddRelativeForce(Vector3.forward * other.speed);
-	//~ rigidbody.rotation = Quaternion(0, other.speed, 0, 0);
-	//~ rigidbody.AddForce(Vector3.right * other.speed * 40);
-	//~ rigidbody.velocity = Vector3( other.speed * 10, 0, 0 );
-    
-}
-
-function OnCollisionEnter(collisionInfo:Collision){
+	myTransform.rotation = Quaternion(0, 180, 0, 0);
 }
